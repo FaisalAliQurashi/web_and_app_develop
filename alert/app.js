739d353ca7a3fc71b250ccf4c0228261a1030218
Faisal Ali <faisal.ali87654321@gmail.com>
2023-10-10 01:34:27 -0700

    javascript Assig 1

---
+++ alert/app.js
@@ -0,0 +1,25 @@
+alert("error! Please enter a valid password")
+alert("Welcome To Js Land...\nHappy Coding")
+alert("Welcome To Js Land...")
+var message="Hello World";
+alert(message);
+var fullname="Jhone Doe"
+var age="15 years Old"
+var certificate="Mubile Appliction Developer"
+alert(fullname);
+alert(age)
+alert(certificate)
+var piza="PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(piza)
+var email="faisal.ali87654321@gmail.com"
+alert("My Email address is "+email)
+var book= "A smarter way to learn JavaScript"
+alert(book);
+document.write("Yah! I Can write Html contant though Javascript")
+var end= "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(end)
+var visitorName="John Doe "
+var product="T-shirt(s) "
+var quantity=" 5 "
+var store="XYZ Clothing store"
+alert(visitorName+"orderd"+quantity+product+"on "+store)
