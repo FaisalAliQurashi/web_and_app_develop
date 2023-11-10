7afb085a25e4e4f9faf0388632d351975898b4bb
Faisal Ali <faisal.ali87654321@gmail.com>
2023-11-10 09:56:55 -0800

    healthcare

--- feedback/app.js
+++ feedback/app.js
@@ -2,8 +2,10 @@
 function post() {
     var name = document.getElementById("exampleFormControlInput1");
     var message = document.getElementById("exampleFormControlTextarea1");
+    var colar= document.getElementById("favcolor");
     console.log(name.value);
     console.log(message.value);
+    console.log(colar.value)
     if(name.value=="" || message.value==""){
         Swal.fire({
             icon: 'error',
@@ -14,8 +16,8 @@ function post() {
         var posts = document.getElementById("post");
         posts.innerHTML += `
             <div class="card" id="card" style="margin-top: 15px;">
-                <h5 class="card-header">FeedBack</h5>
-                <div class="card-body">
+                <h5 class="card-header" style="color: #7bd6f3;">FeedBack</h5>
+                <div class="card-body" style="color:${colar.value};">
                     <h5 class="card-title" id="title">${name.value}</h5>
                     <p class="card-text"  id="text">${message.value}</p>
                 </div>
